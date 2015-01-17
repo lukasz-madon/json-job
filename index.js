22979cad326d1630478fbd97dbf719dee5751e01
lukas madon <lukas@firestartr.co>
2015-01-17 21:17:01 +0000

    Add validator to api call

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var json_job = require('./job');
+var validator = require("z-schema");
+var jsonJob = require('./job');
+var schema = require('./schema')
 
 var app = express();
 
@@ -10,11 +12,14 @@ app.get('/', function(req, res) {
 });
 
 app.get('/api/jobs', function(req, res) {
-	res.jsonp(json_job);
+  res.jsonp(jsonJob);
 });
 
 app.post('/api/validate', function(req, res) {
-	res.send('jobs');
+  validator.validate(jsonJob, schema, function(err, valid) {
+    console.log(arguments)
+    res.json({errros: err, valid: valid});
+  })
 });
 
 app.listen(app.get('port'), function() {

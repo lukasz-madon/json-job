027200893e2399c6e03446f25cffe69a8e54c3ac
lukas madon <lukas@firestartr.co>
2015-01-19 22:23:11 +0000

    Fix typo in index.js
    close #5

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/api/jobs', function(req, res) {
 
 app.post('/api/validate', function(req, res) {
   validator.validate(jsonJob, schema, function(err, valid) {
-    res.json({errros: err, result: valid});
+    res.json({erros: err, result: valid});
   })
 });
 
